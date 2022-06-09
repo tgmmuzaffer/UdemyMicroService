e425f7e7a0476ee65186c3b420f08a7cc8885988
muzaffer <muzaffer.deveci@tazebt.com>
2022-06-10 00:00:01 +0300

    catalog Added

---
+++ UdemyMicroservices/Services/Catalog/FreeCourse.Services.Catalog/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace FreeCourse.Services.Catalog.Models
+{
+    public class Category
+    {
+        [BsonId]//tip belittik
+        [BsonRepresentation(BsonType.ObjectId)]//object ıd si olduğunu verdik mongo db ye
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
---
+++ UdemyMicroservices/Shared/FreeCourse.Shared/FreeCourse.Shared/ControllerBases/CustomBaseController.cs
@@ -0,0 +1,25 @@
+﻿using FreeCourse.Shared.Dtos;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FreeCourse.Shared.ControllerBases
+{
+    public class CustomBaseController  : ControllerBase
+    {
+        //ControllerBase aslında aspnet framework ünde gelen bir class. Bu class a dışardan erişmenin yolu:  Proje>sağ tık=> Edit Project File>
+        //<ItemGroup>
+		//<FrameworkReference Include = "Microsoft.AspNetCore.App"/>
+        //</ItemGroup>
+
+        public IActionResult CreateActionResultInstance<T>(Response<T> response)
+        {
+            return new ObjectResult(response)
+            {
+                StatusCode=response.StatusCode
+            };
+        }
+    }
+}
+
