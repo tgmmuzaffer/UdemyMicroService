b2aabd0a036693e1ecbe4ee91db3a1ccefa878fc
muzaffer <muzaffer.deveci@tazebt.com>
2022-06-15 02:39:13 +0300

    rename folders

---
+++ UdemyMicroservices/Services/Basket/FreeCourse.Services.Basket/Services/IBasketService.cs
@@ -0,0 +1,13 @@
+﻿using FreeCourse.Services.Basket.Dtos;
+using FreeCourse.Shared.Dtos;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Services.Basket.Services
+{
+    public interface IBasketService
+    {
+        Task<Response<BasketDto>> GetBasket(string userid);
+        Task<Response<bool>> SaveOrUpdate(BasketDto basketDto);
+        Task<Response<bool>> DeleteBasket(string userid);
+    }
+}
