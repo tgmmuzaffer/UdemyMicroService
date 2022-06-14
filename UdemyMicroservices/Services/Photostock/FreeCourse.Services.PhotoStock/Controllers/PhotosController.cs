7ae2be16c5ae7866544dca60028f365618afa464
muzaffer <muzaffer.deveci@tazebt.com>
2022-06-14 13:03:52 +0300

    stock api

---
+++ UdemyMicroservices/Services/Photostock/FreeCourse.Services.PhotoStock/Controllers/PhotosController.cs
@@ -0,0 +1,52 @@
+﻿using FreeCourse.Services.PhotoStock.Dtos;
+using FreeCourse.Shared.ControllerBases;
+using FreeCourse.Shared.Dtos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Services.PhotoStock.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PhotosController : CustomBaseController
+    {
+        [HttpPost]
+        public async Task<IActionResult> PhotoSave(IFormFile formFile, CancellationToken cancellationToken)
+        {
+            if(formFile!=null && formFile.Length > 0)
+            {
+                var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/photos", formFile.FileName);
+                using(var stream = new FileStream(path, FileMode.Create))
+                {
+                    await formFile.CopyToAsync(stream, cancellationToken);
+                }
+                var returnpath = "photos/" + formFile.FileName;
+
+                PhotoDto photoDto = new()
+                {
+                    Url = returnpath
+                };
+
+                return CreateActionResultInstance(Response<PhotoDto>.Success(photoDto, 200));
+            }
+
+            return CreateActionResultInstance(Response<PhotoDto>.Fail("fotograf yüklenemedi", 400));
+
+        }
+
+        public  IActionResult PhotoDelete(string photoUrl)
+        {
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/photos", photoUrl);
+            if (!System.IO.File.Exists(path))
+            {
+                return CreateActionResultInstance(Response<NoContent>.Fail("photo not found", 404));
+            }
+            System.IO.File.Delete(path);
+            return CreateActionResultInstance(Response<NoContent>.Success(204));
+
+        }
+    }
+}
---
+++ UdemyMicroservices/Services/Photostock/FreeCourse.Services.PhotoStock/Startup.cs
@@ -0,0 +1,70 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Services.PhotoStock
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+          
+            services.AddControllers(att => 
+            {
+                att.Filters.Add(new AuthorizeFilter()); 
+            });
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "FreeCourse.Services.PhotoStock", Version = "v1" });
+            });
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(opt =>
+            {
+                opt.Authority = Configuration["IdentityServerUrl"];//bu microservice e token ? kimin da?itt??? bilgisini veriyoruz 
+                opt.Audience = "photo_stock_catalog";//En:Audience => Tr:?zleyici(gelen token içindeki audience parametresine bak?cak resource_token bilgisi varsa, tamam sen istek yapabilirsin buraya deyip iizn vericek)
+                //api resource bu identity server daki config de
+
+                opt.RequireHttpsMetadata = false;//http protokülü olarak https bekler defaultta o yüzden false verdik
+
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "FreeCourse.Services.PhotoStock v1"));
+            }
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.UseAuthentication();
+
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
